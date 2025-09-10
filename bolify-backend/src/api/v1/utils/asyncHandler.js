d1e18d713edf501f0f24c3d70f9b88c1201bd9d7
VaradPatel13 <166211979+VaradPatel13@users.noreply.github.com>
2025-09-10 23:24:35 +0530

    Remove bolify-backend folder from repository

--- bolify-backend/src/api/v1/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-// src/api/v1/utils/asyncHandler.js
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-    };
-};
-
-export { asyncHandler };
